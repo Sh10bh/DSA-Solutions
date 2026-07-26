2a58ee94c9a70a9df111823a65670e9a1f3cd2e7
Shubh Gupta <shubhgupta874@gmail.com>
2026-07-26 22:46:27 +0530

    Force committed solution - Time: O(n log n), Space: O(n)

---
+++ 702849-complete-binary-tree-traversal-with-array-input-GFG/702849-complete-binary-tree-traversal-with-array-input.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> levelSort(vector<int>& arr) {
+        vector<vector<int>> res;
+        int n = arr.size();
+        int i = 0, levelSize = 1;
+        while (i < n) {
+            int cnt = min(levelSize, n - i);
+            vector<int> level(arr.begin() + i, arr.begin() + i + cnt);
+            sort(level.begin(), level.end());
+            res.push_back(level);
+            i += cnt;
+            levelSize *= 2;
+        }
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
