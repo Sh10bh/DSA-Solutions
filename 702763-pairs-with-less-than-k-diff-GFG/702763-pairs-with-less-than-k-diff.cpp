600effd79cb83eff07cdac8da6997031ab08f30f
Shubh Gupta <shubhgupta874@gmail.com>
2026-08-04 23:54:16 +0530

    Added solution - Time: O(n log n), Space: O(1)

---
+++ 702763-pairs-with-less-than-k-diff-GFG/702763-pairs-with-less-than-k-diff.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPairs(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end());
+        int count = 0;
+        int left = 0;
+        int n = arr.size();
+        
+        for (int right = 1; right < n; ++right) {
+            while (left < right && arr[right] - arr[left] >= k) {
+                left++;
+            }
+            count += (right - left);
+        }
+        
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
