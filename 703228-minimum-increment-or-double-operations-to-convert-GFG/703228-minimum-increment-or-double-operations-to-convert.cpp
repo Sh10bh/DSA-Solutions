ac5f4b7bce6f5b953d778461ee59c15cd193b356
Shubh Gupta <shubhgupta874@gmail.com>
2026-08-06 17:12:47 +0530

    Added solution - Time: O(n log m), Space: O(1)

---
+++ 703228-minimum-increment-or-double-operations-to-convert-GFG/703228-minimum-increment-or-double-operations-to-convert.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int countMinOperations(vector<int>& arr) {
+        int ops = 0;
+        int max_val = 0;
+        
+        for (int num : arr) {
+            max_val = std::max(max_val, num);
+            while (num > 0) {
+                ops += (num & 1);
+                num >>= 1;
+            }
+        }
+        
+        while (max_val > 1) {
+            ops++;
+            max_val >>= 1;
+        }
+        
+        return ops;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
