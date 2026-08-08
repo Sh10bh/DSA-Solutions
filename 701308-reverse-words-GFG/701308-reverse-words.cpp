3185f05df7a72ba3396adf164ac8f18bd182d4b5
Shubh Gupta <shubhgupta874@gmail.com>
2026-08-08 15:59:29 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701308-reverse-words-GFG/701308-reverse-words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reverseWords(string &s) {
+        vector<string> words;
+        string curr_word = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '.') {
+                if (!curr_word.empty()) {
+                    words.push_back(curr_word);
+                    curr_word = "";
+                }
+            } else {
+                curr_word += s[i];
+            }
+        }
+        
+        if (!curr_word.empty()) {
+            words.push_back(curr_word);
+        }
+        
+        string result = "";
+        
+        for (int i = words.size() - 1; i >= 0; i--) {
+            result += words[i];
+            if (i > 0) {
+                result += ".";
+            }
+        }
+        
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
