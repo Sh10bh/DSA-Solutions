3b6f9c33f5f4be0bc23bede211d27aee027458ba
Shubh Gupta <shubhgupta874@gmail.com>
2026-08-12 16:00:13 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 712197-stock-buy-and-sell-max-one-transaction-allowed-GFG/712197-stock-buy-and-sell-max-one-transaction-allowed.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        // code here
+        int min_price = prices[0];
+        int max_profit = 0;
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] < min_price){
+                min_price = prices[i];
+            }
+            if(prices[i] - min_price > max_profit){
+                max_profit = prices[i] - min_price;
+            }
+        }
+        return max_profit;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
