6aec468ab21e4969f9053f7b7813191144bdd68c
Shubh Gupta <shubhgupta874@gmail.com>
2026-08-13 20:21:47 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 703399-remove-duplicates-from-unsorted-GFG/703399-remove-duplicates-from-unsorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        unordered_set<int> seen;
+        vector<int> result;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (seen.find(arr[i]) == seen.end()) {
+                seen.insert(arr[i]);
+                result.push_back(arr[i]);
+            }
+        }
+        
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
