20671224e990a4b98c7cc5b646bab8d5692f5d9f
Shubh Gupta <shubhgupta874@gmail.com>
2026-08-20 00:21:26 +0530

    Added solution

---
+++ 700227-node-and-ancestor-max-diff-GFG/700227-node-and-ancestor-max-diff.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxDiff(Node* root) {
+        int max_diff = INT_MIN;
+        getMin(root, max_diff);
+        return max_diff;
+    }
+
+private:
+    int getMin(Node* root, int& max_diff) {
+        if (root == nullptr) {
+            return INT_MAX;
+        }
+
+        if (root->left == nullptr && root->right == nullptr) {
+            return root->data;
+        }
+
+        int left_min = getMin(root->left, max_diff);
+        int right_min = getMin(root->right, max_diff);
+
+        int min_descendant = min(left_min, right_min);
+
+        max_diff = max(max_diff, root->data - min_descendant);
+
+        return min(root->data, min_descendant);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
