6207deb61c693f5951af98eae4f2bfd90f528fcc
Shubh Gupta <shubhgupta874@gmail.com>
2026-08-31 19:02:27 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 869719-linked-list-insertion-at-beginning-GFG/869719-linked-list-insertion-at-beginning.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node *insertAtFront(Node *head, int x) {
+        // Code here
+        Node *temp = new Node(x);
+        temp -> next = head;
+        return temp;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
