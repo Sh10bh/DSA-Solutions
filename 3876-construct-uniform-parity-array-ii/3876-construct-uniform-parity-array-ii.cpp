93ed58f901222e01dac60ec56e2ff9071e4935f5
Shubh Gupta <shubhgupta874@gmail.com>
2026-09-03 16:10:08 +0530

    Time: 3 ms (72.51%), Space: 165.9 MB (31.94%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int min_odd = INT_MAX;
+        
+        for (int x : nums1) {
+            if (x % 2 != 0) {
+                min_odd = min(min_odd, x);
+            }
+        }
+        
+        if (min_odd == INT_MAX) {
+            return true;
+        }
+        
+        for (int x : nums1) {
+            if (x % 2 == 0 && x < min_odd) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
