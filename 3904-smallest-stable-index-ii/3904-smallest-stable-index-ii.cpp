037ea707a8d67d79b39e05ff4a75a7aeaf9b308d
Shubh Gupta <shubhgupta874@gmail.com>
2026-09-05 14:43:52 +0530

    Time: 0 ms (100%), Space: 202.6 MB (76.32%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> suffix_min(n);
+        
+        suffix_min[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suffix_min[i] = min(suffix_min[i + 1], nums[i]);
+        }
+        
+        int prefix_max = 0;
+        for (int i = 0; i < n; ++i) {
+            prefix_max = max(prefix_max, nums[i]);
+            
+            if (prefix_max - suffix_min[i] <= k) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
