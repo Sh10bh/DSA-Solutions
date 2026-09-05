20c02ea54fa5ec908579002d19f81b22972804ef
Shubh Gupta <shubhgupta874@gmail.com>
2026-09-05 14:42:14 +0530

    Added solution - Time: O(n + m), Space: O(m)

---
+++ 702683-longest-subsequence-with-adjacent-diff-as-1-GFG/702683-longest-subsequence-with-adjacent-diff-as-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestSubseq(vector<int>& arr) {
+        if (arr.empty()) return 0;
+
+        int max_val = 0;
+        for (int x : arr) {
+            if (x > max_val) {
+                max_val = x;
+            }
+        }
+
+        vector<int> dp(max_val + 2, 0);
+        int max_len = 0;
+
+        for (int x : arr) {
+            int prev_len = (x - 1 >= 0) ? dp[x - 1] : 0;
+            int next_len = (x + 1 <= max_val) ? dp[x + 1] : 0;
+
+            dp[x] = max(prev_len, next_len) + 1;
+
+            if (dp[x] > max_len) {
+                max_len = dp[x];
+            }
+        }
+
+        return max_len;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
