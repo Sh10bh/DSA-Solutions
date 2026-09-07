275e177101d62fc96fa7f91586f009245a7e7776
Shubh Gupta <shubhgupta874@gmail.com>
2026-09-07 12:59:05 +0530

    Time: 5 ms (80.05%), Space: 44.9 MB (47.93%) - LeetHub - Time: O(n log n + m log m), Space: O(1)

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = 0;
+        int j = 0;
+        int n = g.size();
+        int m = s.size();
+        while(i < n && j < m){
+            if(g[i] <= s[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
