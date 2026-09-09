2fef097c7c660d08b11ecc5792f5029126be0f25
Shubh Gupta <shubhgupta874@gmail.com>
2026-09-09 14:58:44 +0530

    Added solution - Time: O(n^2), Space: O(n)

---
+++ 704799-max-digit-sum-number-in-1-to-n-GFG/704799-max-digit-sum-number-in-1-to-n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMax(int n) {
+        string s = to_string(n);
+        int len = s.size();
+        long long best = n;
+        int bestSum = 0;
+        for (char c : s) bestSum += c - '0';
+
+        for (int i = 0; i < len; i++) {
+            if (s[i] == '0') continue;
+            string cand = s.substr(0, i);
+            cand += char(s[i] - 1);
+            cand += string(len - i - 1, '9');
+            long long val = stoll(cand);
+            int sum = 0;
+            for (char c : cand) sum += c - '0';
+            if (sum > bestSum || (sum == bestSum && val > best)) {
+                bestSum = sum;
+                best = val;
+            }
+        }
+        return (int)best;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
